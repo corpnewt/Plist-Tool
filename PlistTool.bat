a037557263b37441987b7b32c9ee5576196842f0
CorpNewt <corporatenewt@gmail.com>
2019-01-02 12:03:11 -0600

    Update PlistTool.bat

--- PlistTool.bat
+++ PlistTool.bat
@@ -1,17 +1,25 @@
 @echo off
 setlocal enableDelayedExpansion
 
-set "arg=%~1"
+REM Setup initial vars
 set "script_name=PlistTool.command"
 set "thisDir=%~dp0"
-echo !thisDir!
+
 REM Get python location
-FOR /F "tokens=* USEBACKQ" %%F IN (`where python`) DO (
+FOR /F "tokens=* USEBACKQ" %%F IN (`where python 2^> nul`) DO (
     SET "python=%%F"
 )
 
+REM Check for py and give helpful hints!
 if /i "!python!"=="" (
-    echo Python is not installed...
+    echo Python is not installed or not found in your PATH var.
+    echo Please install it from https://www.python.org/downloads/windows/
+    echo.
+    echo Make sure you check the box labeled:
+    echo.
+    echo "Add Python X.X to PATH"
+    echo.
+    echo Where X.X is the py version you're installing.
     echo.
     echo Press [enter] to quit.
     pause > nul
@@ -19,8 +27,8 @@ if /i "!python!"=="" (
 )
 
 REM Python found
-if "!arg!"=="" (
+if "%*"=="" (
     "!python!" "!thisDir!!script_name!"
 ) else (
-    "!python!" "!arg!"
+    "!python!" "!thisDir!!script_name!" %*
 )
